0fa031e5fb3a41c7d25c138e0cea7f93b44c5c1e
Nithin Kosanam <nithin.kosanam@gmail.com>
2024-08-20 16:36:02 -0400

    Testing API Key

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const axios = require('axios');
+require('dotenv').config();
+
+let response = null;
+new Promise(async (resolve, reject) => {
+  try {
+    response = await axios.get('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
+      headers: {
+        'X-CMC_PRO_API_KEY': process.env.API_KEY,
+      },
+    });
+  } catch(ex) {
+    response = null;
+    // error
+    console.log(ex);
+    reject(ex);
+  }
+  if (response) {
+    // success
+    const json = response.data;
+    console.log(json);
+    resolve(json);
+  }
+});   
